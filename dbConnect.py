f6a9c8eb979a57fe21325075695dfb092b39dee9
qihua.wu <qihua.wu@tozmart.com>
2019-05-08 18:28:35 +0800

    add video and fix some bugs

--- dbConnect.py
+++ dbConnect.py
@@ -1,4 +1,4 @@
-import json
+import os
 
 import pymysql
 
@@ -6,7 +6,9 @@ conn = pymysql.connect(host="192.168.2.117", port=3306, user="hhh", passwd="1234
 
 cur = conn.cursor()
 
-cur.execute("select tag from style")
+tableName = "Health"
+
+cur.execute("SELECT tag FROM "+tableName)
 
 numbers = len(cur.fetchall())
 
@@ -16,28 +18,46 @@ change_table = {
 
 j = 0
 
-while j < 10:
-    cur.execute("SELECT title FROM Style")
+def createmkdir(path):
+
+    isExists = os.path.exists(path)
+
+    if not isExists:
+
+        os.mkdir(path)
+
+        return True
+
+    else:
+
+        return False
+
+dirpath = '/Users/hymntolife/desktop/test/' + tableName
+
+createmkdir(dirpath)
+
+while j < numbers:
+    cur.execute("SELECT title FROM " + tableName)
 
     title = str(cur.fetchall()[j][0])
 
-    cur.execute("SELECT subtitle FROM Style")
+    cur.execute("SELECT subtitle FROM " + tableName)
 
     subtitle = str(cur.fetchall()[j][0])
 
-    cur.execute("SELECT profilephoto FROM Style")
+    cur.execute("SELECT profilephoto FROM " + tableName)
 
     profilePhoto = str(cur.fetchall()[j][0])
 
-    cur.execute("SELECT author FROM Style")
+    cur.execute("SELECT author FROM " + tableName)
 
     author = str(cur.fetchall()[j][0])
 
-    cur.execute("SELECT date FROM Style")
+    cur.execute("SELECT date FROM " + tableName)
 
     date = str(cur.fetchall()[j][0])
 
-    cur.execute("SELECT text_json FROM Style")
+    cur.execute("SELECT text_json FROM " + tableName)
 
     # print(cur.fetchall()[j][0])
 
@@ -53,8 +73,8 @@ while j < 10:
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
+       
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <title >""" + title + """</title >
     </head>
     <body>
@@ -88,11 +108,12 @@ while j < 10:
         print(value)
 
         if key == "img":
-            message = message + "<img src=\"" + value + "\" class=\"img-fluid\" alt=\"Responsive image\">\n"
+            message = message + "<img data-src=\"" + value + "\" class=\"img-fluid\" alt=\"Responsive image\">\n"
         elif key == "p":
             message = message + "<p>" + value + "</p>\n"
-        # elif key == "video":
-        #     message = message + "<div class=\"video-player\">\n<video class="embed-responsive embed-responsive-16by9" controls="controls" ><source src="video/0.mp4" type="video/mp4"></video>"
+        elif key == "video":
+            message = message + "<div class=\"video-player\">\n<video class=\"embed-responsive embed-responsive-16by9\"" \
+                                " controls=\"controls\" ><source src=\"" + value + "\" type=\"video/mp4\"></video>\n</div>"
         elif key == "li":
             if str(text_json[i-1].keys())[12:-3] != "li":
                 message = message + "<ul>\n<li>" + value + "</li>\n"
@@ -109,71 +130,25 @@ while j < 10:
     message = message + """
     </div>
     </div>
-    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="../js/jquery-2.1.4.min.js"></script>
     <script src="https://cdn.bootcss.com/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.bootcss.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script>
-    var num = document.getElementsByTagName('img').length;
-    var img = document.getElementsByTagName("img");
-    var n = 0; //存储图片加载到的位置，避免每次都从第一张图片开始遍历
-    lazyload(); //页面载入完毕加载可是区域内的图片
-    //window.onscroll = lazyload;
-    function lazyload() { //监听页面滚动事件
-        var seeHeight = document.documentElement.clientHeight; //可见区域高度
-        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; //滚动条距离顶部高度
-        for (var i = n; i < num; i++) {
-            if (img[i].offsetTop < seeHeight + scrollTop) {
-                if (img[i].getAttribute("src") == "") {
-                    img[i].src = img[i].getAttribute("data-src");
-                }
-                n = i + 1;
-            }
-        }
-    }
-	
-	
-	
-// 简单的节流函数
-//fun 要执行的函数
-//delay 延迟
-//time  在time时间内必须执行一次
-function throttle(fun, delay, time) {
-    var timeout,
-        startTime = new Date();
-    return function() {
-        var context = this,
-            args = arguments,
-            curTime = new Date();
-        clearTimeout(timeout);
-        // 如果达到了规定的触发时间间隔，触发 handler
-        if (curTime - startTime >= time) {
-            fun.apply(context, args);
-            startTime = curTime;
-            // 没达到触发间隔，重新设定定时器
-        } else {
-            timeout = setTimeout(function(){
-	            fun.apply(context, args);
-            }, delay);
-        }
-    };
-};
-// 实际想绑定在 scroll 事件上的 handler
-//function lazyload(event) {}
-// 采用了节流函数
-window.addEventListener('scroll',throttle(lazyload,500,1000));
-    </script>
+    <script src="../js/imgAndVideo.js"></script>     
     </body>
     </html>"""
 
     # for (k, v) in change_table.items():
     #     message = message.replace(k, v)
 
-    file = open('/Users/hymntolife/desktop/test/web/' + str(j) +'.html', 'w')
+    file = open('/Users/hymntolife/desktop/test/' + tableName + '/' + title +'.html', 'w')
 
     file.write(message)
 
     j = j + 1
 
+print(j)
 cur.close()
 
 conn.close()
+
+
